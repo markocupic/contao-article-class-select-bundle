537c8ecd838bd1144ccb2ad6d1fa3c039466544b
Marko Cupic <m.cupic@gmx.ch>
2026-05-16 23:35:06 +0200

    Add array return type to getBundles
    Add an explicit ': array' return type to the getBundles method in src/ContaoManager/Plugin.php to make the method signature explicit and improve static typing and compatibility with modern PHP tooling.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -25,7 +25,7 @@ class Plugin implements BundlePluginInterface
     /**
      * {@inheritdoc}
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(MarkocupicContaoArticleClassSelectBundle::class)
